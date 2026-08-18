9574ad90c2555501f94a6b4fa2caf0a6bcde31ce
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-08-18 14:39:41 +0530

    bug: fix tests variables

--- tests/test_io.py
+++ tests/test_io.py
@@ -21,36 +21,35 @@ def error_parquet(tmp_path):
     fake_data.to_csv(bundle_dir / "transcripts.parquet")
     return XeniumBundle(bundle_dir)
 
-
 def test_validation_valid_bundle(mock_bundle):
-    mock_bundle.validate()
+    mock_bundle.validate_transcripts()
 
 def test_validation_bundle_does_not_exist(tmp_path):
     bundle = XeniumBundle(tmp_path / "false_data")
     with pytest.raises(FileNotFoundError, match="Xenium output bundle not found"):
-        bundle.validate()
+        bundle.validate_transcripts()
 
 def test_validation_valid_path(tmp_path):
     transcripts = tmp_path / "transcripts.parquet"
     transcripts.touch()
     bundle = XeniumBundle(transcripts)
     with pytest.raises(ValueError, match="Expected a Xenium output bundle directory"):
-        bundle.validate()
+        bundle.validate_transcripts()
 
 def test_validation_valid_missing_transcripts(tmp_path):
     no_transcripts = tmp_path / "empty_bundle"
     no_transcripts.mkdir()
     bundle = XeniumBundle(no_transcripts)
     with pytest.raises(FileNotFoundError, match="transcripts.parquet not found"):
-        bundle.validate()
+        bundle.validate_transcripts()
 
 def test_valid_transcript_file(mock_bundle):
     assert mock_bundle.transcript_file.is_file()
 
 def test_load_dataframe(mock_bundle):
-    df = mock_bundle.load_dataframe()
+    df = mock_bundle.load_transcripts_dataframe()
     assert isinstance(df, dd.DataFrame)
 
 def test_error_parquet_file(error_parquet):
     with pytest.raises(ValueError, match="Unable to read"):
-        error_parquet.load_dataframe()
+        error_parquet.load_transcripts_dataframe()
