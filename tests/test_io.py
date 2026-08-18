11c603cec7f92f476ebb53f624d6385e5e2d53df
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-08-18 16:54:05 +0530

    tests: add i/o test

--- tests/test_io.py
+++ tests/test_io.py
@@ -6,7 +6,7 @@ from subqcat.io import XeniumBundle
 
 
 @pytest.fixture
-def mock_bundle(tmp_path):
+def mock_transcripts_bundle(tmp_path):
     bundle_dir = tmp_path / "mock_xenium_bundle"
     bundle_dir.mkdir()
     fake_data = pd.DataFrame({"cell_id": [1, 2], "x_location": [10.0, 20.0], "y_location": [15.0, 25.0], "z_location": [13.0, 23.0]})
@@ -14,27 +14,62 @@ def mock_bundle(tmp_path):
     return XeniumBundle(bundle_dir)
 
 @pytest.fixture
-def error_parquet(tmp_path):
+def mock_cell_bundle(tmp_path):
+    bundle_dir = tmp_path / "mock_xenium_bundle"
+    bundle_dir.mkdir()
+    fake_data = pd.DataFrame({"cell_id": [1, 2], "x_location": [10.0, 20.0], "y_location": [15.0, 25.0], "z_location": [13.0, 23.0]})
+    fake_data.to_parquet(bundle_dir / "cells.parquet")
+    return XeniumBundle(bundle_dir)
+
+
+@pytest.fixture
+def error_transcripts_parquet(tmp_path):
     bundle_dir = tmp_path / "error_xenium_bundle"
     bundle_dir.mkdir()
     fake_data = pd.DataFrame({"cell_id": [1, 2], "x_location": [10.0, 20.0], "y_location": [15.0, 25.0], "z_location": [13.0, 23.0]})
     fake_data.to_csv(bundle_dir / "transcripts.parquet")
     return XeniumBundle(bundle_dir)
 
-def test_validation_valid_bundle(mock_bundle):
-    mock_bundle.validate_transcripts()
+@pytest.fixture
+def error_cells_parquet(tmp_path):
+    bundle_dir = tmp_path / "error_xenium_bundle"
+    bundle_dir.mkdir()
+    fake_data = pd.DataFrame({"cell_id": [1, 2], "x_location": [10.0, 20.0], "y_location": [15.0, 25.0], "z_location": [13.0, 23.0]})
+    fake_data.to_csv(bundle_dir / "cells.parquet")
+    return XeniumBundle(bundle_dir)
 
-def test_validation_bundle_does_not_exist(tmp_path):
+
+def test_validation_valid_transcript_bundle(mock_transcripts_bundle):
+    mock_transcripts_bundle.validate_transcripts()
+
+def test_validation_valid_cells_bundle(mock_cell_bundle):
+    mock_cell_bundle.validate_cells()
+
+
+def test_validation_transcripts_bundle_does_not_exist(tmp_path):
     bundle = XeniumBundle(tmp_path / "false_data")
     with pytest.raises(FileNotFoundError, match="Xenium output bundle not found"):
         bundle.validate_transcripts()
 
-def test_validation_valid_path(tmp_path):
+def test_validation_cells_bundle_does_not_exist(tmp_path):
+    bundle = XeniumBundle(tmp_path / "false_data")
+    with pytest.raises(FileNotFoundError, match="Xenium output bundle not found"):
+        bundle.validate_cells()
+
+def test_validation_transcripts_valid_path(tmp_path):
     transcripts = tmp_path / "transcripts.parquet"
     transcripts.touch()
     bundle = XeniumBundle(transcripts)
     with pytest.raises(ValueError, match="Expected a Xenium output bundle directory"):
         bundle.validate_transcripts()
+
+def test_validation_cells_valid_path(tmp_path):
+    cells = tmp_path / "cells.parquet"
+    cells.touch()
+    bundle = XeniumBundle(cells)
+    with pytest.raises(ValueError, match="Expected a Xenium output bundle directory"):
+        bundle.validate_cells()
+
 
 def test_validation_valid_missing_transcripts(tmp_path):
     no_transcripts = tmp_path / "empty_bundle"
@@ -43,13 +78,32 @@ def test_validation_valid_missing_transcripts(tmp_path):
     with pytest.raises(FileNotFoundError, match="transcripts.parquet not found"):
         bundle.validate_transcripts()
 
-def test_valid_transcript_file(mock_bundle):
-    assert mock_bundle.transcript_file.is_file()
+def test_validation_valid_missing_cells(tmp_path):
+    no_cells = tmp_path / "empty_bundle"
+    no_cells.mkdir()
+    bundle = XeniumBundle(no_cells)
+    with pytest.raises(FileNotFoundError, match="cells.parquet not found"):
+        bundle.validate_cells()
 
-def test_load_dataframe(mock_bundle):
-    df = mock_bundle.load_transcripts_dataframe()
+def test_valid_transcript_file(mock_transcripts_bundle):
+    assert mock_transcripts_bundle.transcript_file.is_file()
+
+def test_valid_cells_file(mock_cell_bundle):
+    assert mock_cell_bundle.cells_file.is_file()
+
+def test_load_transcripts_dataframe(mock_transcripts_bundle):
+    df = mock_transcripts_bundle.load_transcripts_dataframe()
     assert isinstance(df, dd.DataFrame)
 
-def test_error_parquet_file(error_parquet):
+def test_load_cells_dataframe(mock_cell_bundle):
+    df = mock_cell_bundle.load_cells_dataframe()
+    assert isinstance(df, dd.DataFrame)
+
+def test_error_parquet_transcripts_file(error_transcripts_parquet):
     with pytest.raises(ValueError, match="Unable to read"):
-        error_parquet.load_transcripts_dataframe()
+        error_transcripts_parquet.load_transcripts_dataframe()
+
+def test_error_parquet_cells_file(error_cells_parquet):
+    with pytest.raises(ValueError, match="Unable to read"):
+        error_cells_parquet.load_cells_dataframe()
+
