ab954764d35170eb00fa3695cf425b7c71e352d9
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-08-22 14:23:56 +0530

    tests: add tests for nearest neighbour transcript calculation

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -7,7 +7,7 @@ from subqcat.subsample import XeniumSampler
 
 
 @pytest.fixture
-def mock_transcripts_bundle(tmp_path):
+def mock_transcripts_singleton_bundle(tmp_path):
     bundle_dir = tmp_path / "mock_xenium_bundle"
     bundle_dir.mkdir()
     fake_data = pd.DataFrame({
@@ -23,6 +23,25 @@ def mock_transcripts_bundle(tmp_path):
     fake_data.to_parquet(bundle_dir / "transcripts.parquet")
     bundle = XeniumBundle(bundle_dir)
     return XeniumSampler(bundle)
+
+@pytest.fixture
+def mock_transcripts_bundle(tmp_path):
+    bundle_dir = tmp_path / "mock_xenium_bundle"
+    bundle_dir.mkdir()
+    fake_data = pd.DataFrame({
+        "cell_id": ["123", "123", "456", "456"], 
+        "x_location": [10.0, 12.0, 100.0, 120.0], 
+        "y_location": [10.0, 10.0, 100.0, 100.0], 
+        "z_location": [10.0, 10.0, 100.0, 100.0], 
+        "qv": [20.0, 20.0, 20.0, 20.0], 
+        "is_gene": [True, True, True, True], 
+        "codeword_index": [1, 1, 1, 1], 
+        "codeword_category": ["gene", "gene", "gene", "gene"]
+    })
+    fake_data.to_parquet(bundle_dir / "transcripts.parquet")
+    bundle = XeniumBundle(bundle_dir)
+    return XeniumSampler(bundle)
+
 
 @pytest.fixture
 def error_mock_transcripts_bundle(tmp_path):
@@ -55,6 +74,15 @@ def test_error_transcript_distance_calc(error_mock_transcripts_bundle):
     with pytest.raises(ValueError, match="Missing required columns for metrics calculation:"):
         compute_metrics = SubcellularMetrics(subsample_df)
         compute_metrics.transcript_distance()
+
+def test_nearest_neighbor_transcript_distance_singleton(mock_transcripts_singleton_bundle):
+    subsampled_df = mock_transcripts_singleton_bundle.subsample()
+    metrics = SubcellularMetrics(subsampled_df)
+    nn_transcripts = metrics.nearest_neighbour_transcript()
+    
+    assert isinstance(nn_transcripts, pd.DataFrame)
+    assert 'mean_nn_distance' in nn_transcripts.columns
+    assert 'cell_id' in nn_transcripts.columns
 
 def test_nearest_neighbor_transcript_distance(mock_transcripts_bundle):
     subsampled_df = mock_transcripts_bundle.subsample()
